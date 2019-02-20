23c1523f2323dc8122019d39ec03652d7fbdb343
wangyong <zhengjun.qi@capgemini.com>
2019-02-20 11:16:29 +0800

    add
    add

---
+++ VideoCaptureAndPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoCaptureAndPlay
+//
+//  Created by 王勇 on 2019/2/20.
+//  Copyright © 2019年 王勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
